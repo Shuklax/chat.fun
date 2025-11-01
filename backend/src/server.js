4fdd1035e9aa8fa3ae793f32b13eafaad9c74ca7
Shuklax <harshshuklaknp@gmail.com>
2025-11-01 12:48:26 +0530

    progress phase 0

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
+import path from 'path';
 
 dotenv.config();
 
@@ -16,7 +17,7 @@ app.use("/api/messages", messageRoutes);
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res)=> {
+    app.get("*", (_ , res)=> {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     })
 }
